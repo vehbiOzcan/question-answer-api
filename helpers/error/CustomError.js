1d79836c25d1f951fe2544b06a46cba14e78e6fe
Vehbi Ozcan <yok>
2023-06-24 00:23:52 +0300

    created CustomError class and express-async-handler pack installed

---
+++ helpers/error/CustomError.js
@@ -0,0 +1,11 @@
+class CustomError extends Error {
+    //Bu sınıf bizim kendi hata türlerimizi oluşturmamızı ve 
+    //yakaladığımız hataları özelleştirmemizi sağlayacak 
+    //Error sınıfını extend ederek tüm hata türlerini yakalyabilmemizi sağlıyor
+    //ayrıca statusu olmayan hatalara da kendimiz status atayabiliyoruz
+    constructor(message,status){
+        super(message);
+        this.status = status;
+    }
+
+}
--- middlewares/error/ErrorHandler.js
+++ middlewares/error/ErrorHandler.js
@@ -1,12 +1,23 @@
+
 //Kendi custom error handlerımızı oluşturduk
+import CustomError from "../../helpers/error/CustomError.js";
+
 const customErrorHandler = (err, req, res, next) => {
-   
-    console.log(err);
+   let customErr = err;
+    console.log(err.name,err.message);
+
+    if(err.name === "SyntaxError"){
+        customErr = new CustomError("Unexpected Syntax", 400)
+    }
+    if(err.name === "ValidationError"){
+        customErr = new CustomError("Validation Error", 400)
+    }
 
     res
-        .status(400)
+        .status(customErr.status || 500)
         .json({
-            success: false
+            success: false,
+            message: customErr.message || "Internal Server Error"
         })
 }
 
